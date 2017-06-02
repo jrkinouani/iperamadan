226cbd19ba258a23d1f4dab75d9ca773e06666e7
jrkinouani <kinouani@gmail.com>
2017-06-02 06:55:39 +0400

    add player video

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def apropos
+  end
+
+  def merci
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+
+  get '/apropos' => "static_pages#apropos"
+
+  get '/merci' => "static_pages#amerci"
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
